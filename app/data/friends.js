36ed40094b7ef489941cf79cb952d6099930172a
Ross Blair <rblair33@gmail.com>
2017-09-15 17:57:07 -0400

    Local file links working correctly and html routes are working

---
+++ app/data/friends.js
@@ -0,0 +1,20 @@
+var friendsData = [{
+  name: "Donald Trump",
+  image: "http://media.breitbart.com/media/2015/12/483208412-real-estate-tycoon-donald-trump-flashes-the-thumbs-up.jpg.CROP_.promo-xlarge2.jpg",
+  scores: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+  totalScore: 10
+},
+{
+  name: "A puppy",
+  image: "http://cdn.akc.org/Marketplace/Breeds/Bernese_Mountain_Dog_SERP.jpg",
+  scores: [5, 5, 5, 5, 5, 5, 5, 5, 5, 5],
+  totalScore: 50
+},
+{
+  name: "Someone's grandma",
+  image: "http://www.ndtv.com/news/grandma%20%283%29.jpg",
+  scores: [3, 3, 3, 3, 3, 2, 2, 2, 2, 2],
+  totalScore: 25
+}];
+
+module.exports = friendsData;
